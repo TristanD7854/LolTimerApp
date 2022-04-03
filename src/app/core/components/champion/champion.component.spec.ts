7db1182d1d02d7eff32e67742ed8197df8608f6d
TristanD7854 <tristan.deniau78@gmail.com>
2022-04-03 17:06:18 +0200

    Add some TUs. Add husky pre-commit hooks

--- src/app/core/components/champion/champion.component.spec.ts
+++ src/app/core/components/champion/champion.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CurrentGameParticipant } from '../../models/riot-api/spectator.model';
+import { ChampionsService } from '../../services/champions/champions.service';
 
 import { ChampionComponent } from './champion.component';
 
@@ -8,17 +11,51 @@ describe('ChampionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ChampionComponent]
+      declarations: [ChampionComponent],
+      providers: [
+        HttpClient,
+        HttpHandler,
+        {
+          provide: ChampionsService,
+          useValue: {
+            getChampionName: () => 'Aatrox'
+          }
+        }
+      ]
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChampionComponent);
     component = fixture.componentInstance;
+
+    // todo : create json for that
+    const mockCurrentGameParticipant: CurrentGameParticipant = {
+      teamId: 100,
+      spell1Id: 4,
+      spell2Id: 12,
+      championId: 161,
+      profileIconId: 4075,
+      summonerName: 'Kreatonn',
+      bot: false,
+      summonerId: 'L1l1eR8qjO-MRqB0BWl4Lh568b17jRVGS96gB4gwprc-WR4',
+      gameCustomizationObjects: [],
+      perks: {
+        perkIds: [8229, 8224, 8210, 8237, 8345, 8347, 5008, 5008, 5003],
+        perkStyle: 8200,
+        perkSubStyle: 8300
+      }
+    };
+    component.currentGameParticipant = mockCurrentGameParticipant;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the correct champion name', () => {
+    expect(component.championName).toEqual('Aatrox');
   });
 });
--- src/app/core/components/search-summoner/search-summoner.component.spec.ts
+++ src/app/core/components/search-summoner/search-summoner.component.spec.ts
@@ -1,6 +1,8 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchSummonerComponent } from './search-summoner.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SearchSummonerComponent', () => {
   let component: SearchSummonerComponent;
@@ -8,7 +10,9 @@ describe('SearchSummonerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SearchSummonerComponent]
+      imports: [RouterTestingModule],
+      declarations: [SearchSummonerComponent],
+      providers: [HttpClient, HttpHandler]
     }).compileComponents();
   });
 
